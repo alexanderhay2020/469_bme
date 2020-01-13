d587f5decc3b0a548752a7e4634335c4617196e0
alex hay <alexanderhay2020@u.northwestern.edu>
2020-01-13 16:02:15 -0600

    I think 1b is correct, stuck on 1c

--- ps1/py/ps1.py
+++ ps1/py/ps1.py
@@ -15,8 +15,12 @@
 # plot(x,y,'.');
 
 import numpy as np
-from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
+import math
+import logging
 import matplotlib.pyplot as plt
+
+from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
+
 
 np.random.seed(1)
 
@@ -46,88 +50,99 @@ def part_a():
     line-by-line translation with same output, modified to fit the assignment
     """
 
-    x = np.random.uniform(low=-10, high=10, size=1000)
-    y = 2*x + np.random.normal(0, 1, x.size)
+    x = np.random.uniform(low=-10, high=10, size=1000) # [1000x1]
+    y = 2*x + np.random.normal(0, 1, x.size) # [1000x1]
 
     fig1 = plt.figure()
-    plt.plot(x,y,".")
+    plt.plot(x,y,".",label="n = 1000")
     plt.title("Part A: Dataset")
-    plt.xlabel("Random Uniform Distribution: n = 1000, -10 < n < 10")
-    plt.ylabel("2*x + e: mu = 0, sigma = 1, size(x)")
+    plt.xlabel("Random Uniform Distribution: -10 < n < 10")
+    plt.ylabel("2*x + e: mu = 0, sigma = 1,q size(x)")
     plt.plot(x,y,".")
+    plt.legend()
 
     return x, y
 
 
-def part_b():
+def part_b(x):
     """
     Place their centers at -12 to 12 at every .5 along the x axis.
     Set the standard deviation of each RBF to 1.
     """
-    rbf_x = np.linspace(-12, 12, 48) # centers at -12 to 12 @ every .5
+    sigma = 1 # standard deviation
+    x_u = np.linspace(-12, 12, 48) # centers at -12 to 12 @ every .5 [48x1]
 
-    rbf_y = np.random.normal(0,1,rbf_x.size)
-    rbf = Rbf(rbf_x, rbf_y, function="gaussian")
+    h = np.zeros((1000,48)) # [1000x48]
+    for j in range(len(x_u)): # 1-48
+        for i in range(len(x)): # 1-1000
+            eqn = (x[i]-x_u[j])**2
+            # eqn = (-1/(2*(sigma**2)))*(eqn_x)
 
-    xi = np.linspace(-12, 12, 1001)
-    yi = rbf(xi)
+            h[i,j] = math.exp(-eqn)
 
     fig2 = plt.figure()
     plt.title("Part B: Radial Basis Functions")
-    plt.plot(rbf_x, rbf_y, '.')
-    plt.plot(xi, yi, 'b')
+    plt.plot(x_u, h.T)#,":")
     plt.xlabel("n = 48, -12 < n < 12")
     plt.ylabel("axis=0, mu = 0, sigma = 1, size(x)")
+    plt.ylim(bottom = 0)
+    plt.ylim(top = 1)
+    # plt.legend()
+    return h # [1000x48]
 
-    return rbf_x, rbf_y
-
-
-def part_c(x, y, rbf_x, rbf_y):
+def part_c(x, y, h):
     """
-    text
+    x    [1000x1]
+    y    [1000x1]
+    h    [1000x48]
+    f(x) [1000,1]
+
+    PARAMETERS:
+    r = 1 - standard deviation
+    c = x_u - center
+    w = randomized weight vector [48x1]
     """
-    rbf_x = np.expand_dims(rbf_x, axis=0) # size: 1x1000 (RxC)
+    w = np.random.random((48)) # starting weights [48x1]
+    w0 = w
+    sum = 0
+    f_x = np.zeros((1000))
 
-    for i in range(1000):
-        """
-        neuron
-        """
-        input = rbf_x # size [1x48]
-        weights = np.random.random((48,1000)) # size [48x1000]
+    for i in range(1,1000):
+        sum = sum + np.dot(h, w)
+    print f_x.shape
 
-        # print ("weights shape: " + str(weights.shape))
-        # print ("rbf_x shape: " + str(rbf_x.shape))
+    # for i in range(48):
+        # print h[i,:].shape
+        # f_x = f_x + np.dot(h[i,:], w)
+        # print f_x.shape
+    # for i in range(1000):
+    #     h_x = np.dot(h, w) #[1000x1] = [1000x48]*[48x1]
+    #     h_x = sigmoid(y_n)
+    #     error = y - h_x
+    #     adjustments = error * sigmoid_derivative(h_x)
+    #     w = w + np.dot(h.T, adjustments)
+    #
+    #
+    # print("initial weights: " + str(w0))
+    # print
+    # print("final weights: " + str(w))
+    # print
+    # print("difference: " + str(w-w0))
 
-        in_w = np.dot(rbf_x, weights) # [1x48]*[48*1000]=[1x1000]
-        # print ("xw shape: " + str(xw.shape))
+    # fig3 = plt.figure()
+    # plt.plot(x, y_n)
+    # plt.ylim(bottom = -20)
+    # plt.ylim(top = 20)
+    # plt.plot(error)
+    # print h_x.shape
 
-        training_output = y
-        output = sigmoid(in_w)
-
-        error = training_output - output
-
-        if (i==0):
-            # print ("training output: " + str(training_output.shape))
-            print ("output: " + str(output))
-            print ("error: " + str(error))
-            print
-
-        if (i==999):
-            # print ("training output: " + str(training_output.shape))
-            print ("output: " + str(output))
-            print ("error: " + str(error))
-            print
-
-        adjustments = error * sigmoid_derivative(output)
-
-        weights = weights + np.dot(rbf_x.T,adjustments)
 
 
 def main():
     x, y = part_a()
-    rbf_x, rbf_y = part_b()
-    part_c(x, y, rbf_x, rbf_y)
-    # plt.show()
+    h = part_b(x)
+    part_c(x, y, h)
+    plt.show()
 
 if __name__ == '__main__':
     main()
