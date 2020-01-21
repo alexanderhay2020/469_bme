cac46ba4f00394bd4d2c1479a8e32355801a674f
alex hay <alexanderhay2020@u.northwestern.edu>
2020-01-21 15:45:55 -0600

    2b works with comments

--- ps1/py/part2.py
+++ ps1/py/part2.py
@@ -52,7 +52,6 @@ def part_2a():
     **********************************************
     create training dataset
     **********************************************
-<<<<<<< HEAD
 
     matlab dataset:
 
@@ -61,8 +60,6 @@ def part_2a():
     dat = [dat1; dat2];
 
     y = [ones(100,1); -1*ones(100,1)];   % these are the labels for the classes
-=======
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
 
     tempx = np.random.normal(6,2,size=100)
@@ -77,29 +74,20 @@ def part_2a():
 
     y = np.expand_dims(np.append(np.ones((100,1)),-1*np.ones((100,1))),axis=1) # [200x1]
 
-<<<<<<< HEAD
     """
     shuffle dataset
     """
-=======
-    # shuffle dataset
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     temp = np.append(dat,y,axis=1)
     np.random.shuffle(temp)
     dat = temp[:,:2]
     tempy = temp[:,-1]
     y = np.expand_dims(tempy,axis=1)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
     **********************************************
     create validation dataset
     **********************************************
     """
-<<<<<<< HEAD
     v_tempx = np.random.normal(6,2,size=10)
     v_tempy = np.random.normal(2,1,size=10)
     v_dat1 = np.array((v_tempx,v_tempy)).T
@@ -120,27 +108,6 @@ def part_2a():
     v_dat = temp[:,:2]
     v_tempy = temp[:,-1]
     v_y = np.expand_dims(v_tempy,axis=1)
-=======
-
-    tempx = np.random.normal(6,2,size=10)
-    tempy = np.random.normal(2,1,size=10)
-    dat1 = np.array((tempx,tempy)).T
-
-    tempx = np.random.normal(2,3,size=10)
-    tempy = np.random.normal(8,1,size=10)
-    dat2 = np.array((tempx,tempy)).T
-
-    dat = np.append(dat1, dat2, axis=0) # [200x2]
-
-    y = np.expand_dims(np.append(np.ones((10,1)),-1*np.ones((10,1))),axis=1) # [20x1]
-
-    # shuffle dataset
-    temp = np.append(dat,y,axis=1)
-    np.random.shuffle(temp)
-    dat = temp[:,:2]
-    tempy = temp[:,-1]
-    y = np.expand_dims(tempy,axis=1)
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
 
     """
@@ -169,37 +136,21 @@ def part_2a():
         """
         neuron
         """
-<<<<<<< HEAD
         input = dat
         input = np.append(input,bias,axis=1)
         xw = np.dot(input,w) # [4x3]*[3*1]=[4x1]
-=======
-        dat = np.append(dat,bias,axis=1)
-        xw = np.dot(dat,w) # [4x3]*[3*1]=[4x1]
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
         y_hat = sign(xw)
 
-<<<<<<< HEAD
         error = y - y_hat
-=======
-        error = y - output
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
         error_arr[i] = sum(error)
 
         adjustments = error * sign_derivative(y_hat)
 
-<<<<<<< HEAD
         w = w + np.dot(input.T,adjustments)
         # w = normalize(w)
 
     print "Weights after training: "
-=======
-        w = w + np.dot(dat.T,adjustments)
-        # w = normalize(w)
-
-    print "weights after training: "
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     print w
     print
 
@@ -208,12 +159,7 @@ def part_2a():
     print percent_error[0]
     print
 
-<<<<<<< HEAD
     y_hat = np.dot(input,w)
-=======
-    y_hat = np.dot(dat,w)
-
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
     """
     **********************************************
@@ -223,13 +169,8 @@ def part_2a():
     fig1 = plt.figure()
     plt.plot(dat1.T[0], dat1.T[1], "ro", label="dat1")
     plt.plot(dat2.T[0], dat2.T[1], "bo", label="dat2")
-<<<<<<< HEAD
     # plt.plot(np.cross(w.T,input))
     plt.plot((sum(y-np.round(y_hat,0))/epochs)*100,"wo",label="percent error: " + str(percent_error))# + str((sum(y-np.round(y_hat,0))/200)*100))
-=======
-    # plt.plot(np.cross(w.T,dat))
-    plt.plot((sum(y-np.round(output,0))/epochs)*100,"wo",label="percent error: " + str(percent_error))# + str((sum(y-np.round(output,0))/200)*100))
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     for i in range(len(y_hat)):
     	if (y_hat[i]<0):
     		plt.plot(dat[i][0], dat[i][1],"b.")
@@ -255,7 +196,6 @@ def part_2b():
     **********************************************
     create training dataset
     **********************************************
-<<<<<<< HEAD
 
     matlab dataset:
 
@@ -264,8 +204,6 @@ def part_2b():
     dat = [dat1; dat2];
 
     y = [ones(100,1); 0*ones(100,1)];  % labels for classes
-=======
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
     tempx = np.random.normal(6,2,size=100)
     tempy = np.random.normal(2,1,size=100)
@@ -279,29 +217,20 @@ def part_2b():
 
     y = np.expand_dims(np.append(np.ones((100,1)),0*np.ones((100,1))),axis=1) # [200x1]
 
-<<<<<<< HEAD
     """
     shuffle dataset
     """
-=======
-    # shuffle dataset
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     temp = np.append(dat,y,axis=1)
     np.random.shuffle(temp)
     dat = temp[:,:2]
     tempy = temp[:,-1]
     y = np.expand_dims(tempy,axis=1)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
     **********************************************
     create validation dataset
     **********************************************
     """
-<<<<<<< HEAD
     v_tempx = np.random.normal(6,2,size=10)
     v_tempy = np.random.normal(2,1,size=10)
     v_dat1 = np.array((v_tempx,v_tempy)).T
@@ -322,26 +251,6 @@ def part_2b():
     v_dat = v_temp[:,:2]
     v_tempy = v_temp[:,-1]
     v_y = np.expand_dims(v_tempy,axis=1)
-=======
-    tempx = np.random.normal(6,2,size=10)
-    tempy = np.random.normal(2,1,size=10)
-    dat1 = np.array((tempx,tempy)).T
-
-    tempx = np.random.normal(2,3,size=10)
-    tempy = np.random.normal(8,1,size=10)
-    dat2 = np.array((tempx,tempy)).T
-
-    dat = np.append(dat1, dat2, axis=0) # [200x2]
-
-    y = np.expand_dims(np.append(np.ones((10,1)),0*np.ones((10,1))),axis=1) # [200x1]
-
-    # shuffle dataset
-    temp = np.append(dat,y,axis=1)
-    np.random.shuffle(temp)
-    dat = temp[:,:2]
-    tempy = temp[:,-1]
-    y = np.expand_dims(tempy,axis=1)
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
 
     """
@@ -349,11 +258,7 @@ def part_2b():
     network parameters
     **********************************************
     """
-<<<<<<< HEAD
     epochs = 50
-=======
-    epochs=30
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     w = np.random.random((3,1)) # starting weight for each column (synapse)
     bias = np.ones((len(dat),1)) # bias
     error_arr = np.zeros((epochs,1))
@@ -364,11 +269,7 @@ def part_2b():
     perceptron single layer network
     **********************************************
     """
-<<<<<<< HEAD
     print "Weights Before Training: "
-=======
-    print "Starting weights: "
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     print w
     print
 
@@ -376,38 +277,21 @@ def part_2b():
         """
         neuron
         """
-<<<<<<< HEAD
         input = dat
         input = np.append(input,bias,axis=1)
         xw = np.dot(input,w) # [4x3]*[3*1]=[4x1]
-=======
-
-        dat = np.append(dat,bias,axis=1)
-        xw = np.dot(dat,w) # [4x3]*[3*1]=[4x1]
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
         y_hat = sigmoid(xw)
 
-<<<<<<< HEAD
         error = y - y_hat
-=======
-        error = y - output
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
         error_arr[i] = sum(error)
 
         adjustments = error * sigmoid_derivative(y_hat)
 
-<<<<<<< HEAD
         w = w + np.dot(input.T,adjustments)
         # w = normalize(w)
 
     print "Weights After Training: "
-=======
-        w = w + np.dot(dat.T,adjustments)
-        # w = normalize(w)
-
-    print "weights after training: "
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     print w
     print
 
@@ -416,15 +300,9 @@ def part_2b():
     print percent_error[0]
     print
 
-<<<<<<< HEAD
     y_hat = np.dot(input,w)
 
 
-=======
-    y_hat = np.dot(dat,w)
-
-
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
     **********************************************
     plotting
@@ -433,12 +311,7 @@ def part_2b():
     fig1 = plt.figure()
     plt.plot(dat1.T[0], dat1.T[1], "ro", label="dat1")
     plt.plot(dat2.T[0], dat2.T[1], "bo", label="dat2")
-<<<<<<< HEAD
     # plt.plot((sum(y-np.round(y_hat,0))/epochs)*100,"wo",label="percent error: " + str(percent_error))# + str((sum(y-np.round(y_hat,0))/200)*100))
-=======
-    # plt.plot(np.cross(w.T,dat))
-    plt.plot((sum(y-np.round(output,0))/epochs)*100,"wo",label="percent error: " + str(percent_error))# + str((sum(y-np.round(output,0))/200)*100))
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     for i in range(len(y_hat)):
     	if (y_hat[i]<0):
     		plt.plot(dat[i][0], dat[i][1],"b.")
@@ -464,7 +337,6 @@ def part_2c():
     create training dataset
     **********************************************
 
-<<<<<<< HEAD
     matlab dataset:
 
     dat1 = [normrnd(6,1,100,1) normrnd(2,1,100,1)];      % create the input data
@@ -475,12 +347,6 @@ def part_2c():
     y(:,1) = [ones(100,1); zeros(100,1); zeros(100,1)];   % the class labels as three dimensional outputs
     y(:,2) = [zeros(100,1); ones(100,1); zeros(100,1)];
     y(:,3) = [zeros(100,1); zeros(100,1); ones(100,1)];
-=======
-    """
-    **********************************************
-    create training dataset
-    **********************************************
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     """
     tempx = np.random.normal(6,2,size=100)
     tempy = np.random.normal(2,1,size=100)
@@ -497,15 +363,6 @@ def part_2c():
     dat = np.append(dat1, dat2, axis=0) # [300x2]
     dat = np.append(dat, dat3, axis=0)
 
-<<<<<<< HEAD
-=======
-    """
-    matlab code
-    y(:,1) = [ones(100,1); zeros(100,1); zeros(100,1)];   % the class labels as three dimensional outputs
-    y(:,2) = [zeros(100,1); ones(100,1); zeros(100,1)]';
-    y(:,3) = [zeros(100,1); zeros(100,1); ones(100,1)]';
-    """
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     col1 = np.append(np.ones((100,1)),np.zeros((200,1)),axis=0)
     col2 = np.append(np.zeros((100,1)),np.ones((100,1)),axis=0)
     col2 = np.append(col2,np.zeros((100,1)),axis=0)
@@ -514,18 +371,13 @@ def part_2c():
     y = np.append(col1,col2,axis=1)
     y = np.append(y,col3,axis=1)
 
-<<<<<<< HEAD
     """
     shuffle dataset
     """
-=======
-    # shuffle dataset
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     # temp = np.append(dat,y,axis=1)
     # np.random.shuffle(temp)
     # dat = temp[:,:2]
     # tempy = temp[:,-3:]
-<<<<<<< HEAD
 
     print y
 
@@ -573,32 +425,12 @@ def part_2c():
     **********************************************
     """
     epochs = 30
-=======
-
-    """
-    **********************************************
-    network parameters
-    **********************************************
-    """
-    # print y
-    epochs = 3000
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     w = np.random.random((3,3)) # starting weight for each column (synapse)
     bias = np.ones((len(dat),1)) # bias
     error_arr = np.zeros((epochs,3))
     max=0
 
-<<<<<<< HEAD
     print "Starting Weights: "
-=======
-
-    """
-    **********************************************
-    perceptron single layer network
-    **********************************************
-    """
-    print "Starting weights: "
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     print w
     print
 
@@ -612,18 +444,12 @@ def part_2c():
         """
         neuron
         """
-<<<<<<< HEAD
         input = dat
         input = np.append(input,bias,axis=1)
         xw = np.dot(input,w)
-=======
-        dat = np.append(dat,bias,axis=1)
-        xw = np.dot(dat,w)
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
         y_hat = sigmoid(xw)
 
-<<<<<<< HEAD
         error = y - y_hat
         # print y.shape
         # error_arr[i] = sum(error[i])
@@ -642,35 +468,6 @@ def part_2c():
     print
 
     y_hat = np.dot(input,w)
-=======
-        error = y - output
-        for j in range(len(output)):
-            error_arr[i] = sum(error[j])
-
-        adjustments = error * sigmoid_derivative(output)
-
-        w = w + np.dot(dat.T,adjustments)
-        # w = normalize(w)
-
-    print "weights after training: "
-    print w
-    print
-
-    print error.shape
-
-    percent_error=(sum(y-np.round(output,0))/epochs)*100
-    # print percent_error[0]
-    for i in range(len(percent_error)):
-        percent_error = abs(np.round(percent_error,2))
-    print "percent error y_1:" + '%.2f' % percent_error[0]
-    print
-    print "percent error y_2:" + '%.2f' % percent_error[1]
-    print
-    print "percent error y_3:" + '%.2f' % percent_error[2]
-    print
-
-    y_hat = np.dot(dat,w)
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
     for i in range(y_hat.shape[0]):
 
@@ -703,20 +500,13 @@ def part_2c():
     plotting
     **********************************************
     """
-<<<<<<< HEAD
     print y_hat
 
-=======
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
     fig1 = plt.figure()
     plt.plot(dat1.T[0], dat1.T[1], "ro", label="dat1")
     plt.plot(dat2.T[0], dat2.T[1], "bo", label="dat2")
     plt.plot(dat3.T[0], dat3.T[1], "go", label="dat3")
-<<<<<<< HEAD
     # plt.plot((sum(y-np.round(y_hat,0))/epochs)*100,"wo",label="percent error: " + str(np.round(percent_error,2)))# + str((sum(y-np.round(y_hat,0))/200)*100))
-=======
-    plt.plot((sum(y-np.round(output,0))/epochs)*100,"wo",label="percent error: " + str(percent_error))# + str((sum(y-np.round(output,0))/200)*100))
->>>>>>> 1188396d3d83d31cffe8daeeab3ea8a744bf5f0e
 
     for i in range(len(y_hat)):
         if (y_hat[i][0]==1):
